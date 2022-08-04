582e000e3037cd3c791e01263d2090a70455207f
praveen <praveenpkbadiger@gmail.com>
2022-08-04 15:30:14 +0530

    adding logical programs

---
+++ bridgelab/PerfectNumber.java
@@ -0,0 +1,32 @@
+package bridgelab;
+
+import java.util.Scanner;
+
+public class PerfectNumber
+{
+    public static void main(String[] args)
+    {
+
+        int n; //initialize a number(n)
+        int sum = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number:");
+
+        n = sc.nextInt();
+        for (int i = 1; i < n; i++)
+        {
+            if (n % i == 0)
+            {
+                sum = sum + i;
+                System.out.println(sum);
+            }
+        }
+        if (sum == n)
+        {
+            System.out.println("enter no is perfect ");
+        } else
+            System.out.println("Enter no is not perfect");
+        sc.close();
+    }
+}
+
